65ae1d7863bc8a25f8656c1971e556b8a33b3bc1
booky10 <boooky10@gmail.com>
2022-11-11 22:38:08 +0100

    Added more target arguments to the vanish commands

--- src/main/java/dev/booky/vanish/commands/VanishCommand.java
+++ src/main/java/dev/booky/vanish/commands/VanishCommand.java
@@ -5,10 +5,12 @@ import dev.booky.vanish.VanishManager;
 import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandTree;
 import dev.jorel.commandapi.arguments.LiteralArgument;
+import dev.jorel.commandapi.arguments.PlayerArgument;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.util.TriState;
 import org.bukkit.NamespacedKey;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataType;
 
@@ -36,10 +38,16 @@ public final class VanishCommand {
         new CommandTree(MAIN_NAME)
                 .withAliases(ALIASES.toArray(String[]::new))
                 .withPermission("cloudvanish.command")
-                .executesPlayer((Player player, Object[] args) -> toggleVanish(player))
+                .executesPlayer((Player player, Object[] args) -> toggleVanish(player, player))
+                .then(new PlayerArgument("target")
+                        .withPermission("cloudvanish.command.other")
+                        .executes((CommandSender sender, Object[] args) -> toggleVanish(sender, (Player) args[0])))
                 .then(new LiteralArgument("pickup")
                         .withPermission("cloudvanish.command.pickup")
-                        .executesPlayer((Player player, Object[] args) -> togglePickup(player)))
+                        .executesPlayer((Player player, Object[] args) -> togglePickup(player, player))
+                        .then(new PlayerArgument("target")
+                                .withPermission("cloudvanish.command.pickup.other")
+                                .executes((CommandSender sender, Object[] args) -> togglePickup(sender, (Player) args[0]))))
                 .register();
     }
 
@@ -49,22 +57,47 @@ public final class VanishCommand {
         }
     }
 
-    private void toggleVanish(Player player) {
+    private void toggleVanish(CommandSender sender, Player player) {
+        boolean couldSee = sender instanceof Player && ((Player) sender).canSee(player);
         TriState result = this.manager.toggleVanish(player);
 
         // We only have to cover this case, otherwise a "broadcast" message will be sent anyway
         if (result == TriState.NOT_SET) {
-            player.sendMessage(VanishManager.getPrefix().append(Component.translatable("vanish.command.toggle.fail", NamedTextColor.RED)));
+            sender.sendMessage(VanishManager.getPrefix().append(Component.translatable("vanish.command.toggle.fail", NamedTextColor.RED)));
+            return;
+        }
+
+        // check if the target is the same as the sender, or if the sender isn't an actual player
+        if (sender == player || !(sender instanceof Player playerSender)) {
+            return;
+        }
+
+        if (result == TriState.TRUE) { // vanished
+            if (!playerSender.canSee(player)) {
+                sender.sendMessage(VanishManager.getPrefix()
+                        .append(Component.translatable()
+                                .color(NamedTextColor.YELLOW)
+                                .key("vanish.vanished")
+                                .args(player.teamDisplayName())
+                                .build()));
+            }
+        } else if (!couldSee) { // unvanished
+            sender.sendMessage(VanishManager.getPrefix()
+                    .append(Component.translatable()
+                            .color(NamedTextColor.YELLOW)
+                            .key("vanish.unvanished")
+                            .args(player.teamDisplayName())
+                            .build()));
         }
     }
 
-    private void togglePickup(Player player) {
+    private void togglePickup(CommandSender sender, Player player) {
         if (player.getPersistentDataContainer().has(this.pickupKey)) {
             player.getPersistentDataContainer().remove(this.pickupKey);
-            player.sendMessage(VanishManager.getPrefix().append(Component.translatable("vanish.command.pickup.off", NamedTextColor.YELLOW)));
+            sender.sendMessage(VanishManager.getPrefix().append(Component.translatable("vanish.command.pickup.off", NamedTextColor.YELLOW)));
         } else {
             player.getPersistentDataContainer().set(this.pickupKey, PersistentDataType.BYTE, (byte) 0);
-            player.sendMessage(VanishManager.getPrefix().append(Component.translatable("vanish.command.pickup.on", NamedTextColor.YELLOW)));
+            sender.sendMessage(VanishManager.getPrefix().append(Component.translatable("vanish.command.pickup.on", NamedTextColor.YELLOW)));
         }
     }
 }
